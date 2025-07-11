971bfede585ae7cf8aa537d18c87d76ce4f5a08e
Ian Boston <ieb@tfd.co.uk>
2025-07-11 15:13:13 +0100

    Fixes to dist

--- ui/wwwroot/make-dist-cache.sh
+++ ui/wwwroot/make-dist-cache.sh
@@ -8,5 +8,6 @@ cp -r src/n2k dist/n2k
 cp -r src/navtex dist/navtex
 rm -rf dist/lifepo4/node_modules dist/n2k/node_modules dist/navtex/node_modules
 cd dist
-# tar --no-mac-metadata --no-xattrs  --exclude navtex/cache -c -z -f ../dist.tgz .
-gtar --no-xattrs  --exclude navtex/cache -c -z -f ../dist.tgz .
+# macOS tar may add unwanted metadata and SCHILY flags.
+#tar --no-mac-metadata --no-xattrs  -c -z -f ../dist-cache.tgz navtex/cache
+gtar  --no-xattrs  -c -z -f ../dist-cache.tgz navtex/cache
